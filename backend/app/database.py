4f717fb1ca24e831d32d3ba88a3c433a10655f37
baitueycho-maker <baitueycho@gmail.com>
2026-08-03 15:11:50 +0700

    ตรวจจับมือ 2 ข้าง

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,12 +1,10 @@
-
 # app/database.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # เปลี่ยน username, password, localhost, port, dbname ให้ตรงกับเครื่องของคุณนะครับ
-DATABASE_URL = "postgresql://postgres:6610210687@localhost:5432/thai_sign_learning"
-
+DATABASE_URL = "postgresql://postgres:6610210001@localhost:5432/thai_sign_learning"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
